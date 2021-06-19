9909d59f35e670ee9a7cc15d347b70f91883d693
skabhi003 <abhishek140.123@gmail.com>
2021-06-19 09:47:15 +0530

    image added

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Linux World!  <b>Hello</b>"
+    return "Linux World!  <b>Hello</b> <img src='https://myimagest.blob.core.windows.net/myclwimage/my.jpg' />"
